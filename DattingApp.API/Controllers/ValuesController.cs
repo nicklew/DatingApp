3c663a2295ee7d896be50760b3f6b4266b986248
NickLew <NickLew@gmail.com>
2019-11-11 19:24:46 -0800

    End of Section 4

--- DattingApp.API/Controllers/ValuesController.cs
+++ DattingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DattingApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
